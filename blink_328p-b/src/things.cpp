555c756916cd25676b82bca17213d1371870e5d3
wa1tnr <wa1tnr@users.noreply.github.com>
2024-01-16 13:49:21 +0000

    clang-format blindly applied
    	modified:   things.cpp
    
    On branch asmloop-aa

--- blink_328p-b/src/things.cpp
+++ blink_328p-b/src/things.cpp
@@ -7,22 +7,20 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-   void start();
-   void vmain();
+void start();
+void vmain();
 #ifdef __cplusplus
 }
 #endif
 
-void setup_serial(void) {
-  Serial.begin(9600);
-}
+void setup_serial(void) { Serial.begin(9600); }
 
 void as_slowest(void) {
-        for (volatile long i = 544; i > 0; i--) {
-            for (volatile long count = 88; count > 1; count--) {
-              ; // no operation
-            }
+    for (volatile long i = 544; i > 0; i--) {
+        for (volatile long count = 88; count > 1; count--) {
+            ; // no operation
         }
+    }
 }
 void slowest(void) { // 25, 744, 88 not terrible - six slow counts
     for (volatile long j = 25; j > 0; j--) {
@@ -30,49 +28,54 @@ void slowest(void) { // 25, 744, 88 not terrible - six slow counts
     }
 }
 
-
 void slower(void) {
-  for (volatile long count = 123; count > 1; count--) {
-    ; // no operation
-  }
+    for (volatile long count = 123; count > 1; count--) {
+        ; // no operation
+    }
 }
 
-
-void end_serial(void) {
-  Serial.end();
-}
+void end_serial(void) { Serial.end(); }
 
 void setup(void) {
-  setup_serial();
-  Serial.println("  CARTUHN CAYVERS, GMBH   Tue 16 Jan 13:07:19 UTC 2024");
-  Serial.print("     IN setup();");
-  Serial.print("  start();  ");
-  start();
-  delay(700);
-  Serial.print("  vmain();  ");
+    setup_serial();
+    Serial.println("  CARTUHN CAYVERS, GMBH   Tue 16 Jan 13:07:19 UTC 2024");
+    Serial.print("     IN setup();");
+    Serial.print("  start();  ");
+    start();
+    delay(700);
+    Serial.print("  vmain();  ");
 }
 
 void loop(void) {
-  end_serial();
-  slower();
-  vmain();
-  slower();
-  setup_serial();
-  Serial.write('.');
-  slowest();
-  slowest();
+    end_serial();
+    slower();
+    vmain();
+    slower();
+    setup_serial();
+    Serial.write('.');
+    slowest();
+    slowest();
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// - - - - - - - - - - - - - - -                               - - - - - - - - - - - - - - - - - - - - - -
-// - - - - - - - - - - - - - - -     crufty von cruftheimer    - - - - - - - - - - - - - - - - - - - - - -
-// - - - - - - - - - - - - - - -                               - - - - - - - - - - - - - - - - - - - - - -
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - -                               - - - - - - - - -
+// - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - -     crufty von cruftheimer    - - - - - - - - -
+// - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - -                               - - - - - - - - -
+// - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - -
 // - - -
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - -
 
 // void slower(void) {
 // count = 1233445;
